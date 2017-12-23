e906868d054cf5d77249b7fd22323d2ac4f149a5
Eden Mesfun <emesfun@gmail.com>
2017-12-23 18:46:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,3 @@
-=begin
-We expect fizzbuzz(3) to return "Fizz"
-We expect fizzbuzz(5) to return "Buzz"
-We expect fizzbuzz(15) to return "FizzBuzz"
-We expect fizzbuzz(4) to return nil.
-=end
-
-
 def fizzbuzz(x)
   if x % 3 == 0 && x % 5 == 0 
     return "FizzBuzz"
